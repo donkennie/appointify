cc2726ee02059460c1d3f03f17c270019ef59d73
donkennie <ajeigbekehinde160@gmail.com>
2025-06-04 03:48:31 +0100

    complete the doctors page

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { AppContext } from '../contexts/AppContext'
 
 const Appointment = () => {
-  return (
-    <div>Appointment</div>
+
+  const {docId} = useParams()
+  const {doctors} = useContext(AppContext)
+
+  const [docInfo, setDocInfo] = useState()
+
+  const fetchDocInfo = async () => {
+    const docInfo = doctors.find(doc => doc._id === docId)
+    setDocInfo(docInfo)
+  }
+
+  useEffect(() => {
+    fetchDocInfo()
+  }, [doctors, docId])
+
+  return docInfo && (
+    <div>
+      <div>
+        <div>
+          <img src={docInfo.image} alt="" />
+        </div>
+
+        <div>
+          
+        </div>
+      </div>
+    </div>
   )
 }
 
