c794ba91fbec53938aef0beca0a906628b7b09c5
donkennie <ajeigbekehinde160@gmail.com>
2025-06-08 06:24:11 +0100

    add related doctors

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -2,11 +2,13 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { AppContext } from '../contexts/AppContext'
 import { assets } from '../assets/assets'
+import RelatedDoctors from '../components/RelatedDoctors'
 
 const Appointment = () => {
 
   const {docId} = useParams()
   const {doctors, currencySymbol} = useContext(AppContext)
+  const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
 
   const [docInfo, setDocInfo] = useState(null)
 
@@ -26,14 +28,14 @@ const Appointment = () => {
     let today = new Date() 
     for(let i = 0; i < 7; i++){
       let currentDate = new Date(today)
-      currentDate.setDate(today.getDate() + 1)
+      currentDate.setDate(today.getDate() + i)
 
       let endTime = new Date()
-      endTime.setDate(today.getDate() + 1)
+      endTime.setDate(today.getDate() + i)
       endTime.setHours(21, 0, 0, 0)
 
       if(today.getDate() === currentDate.getDate()){
-        currentDate.set(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10)
+        currentDate.setHours(currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10)
         currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0)
       }
       else{
@@ -74,7 +76,7 @@ const Appointment = () => {
     <div>
       <div className='flex flex-col sm:flex-row gap-4'>
         <div>
-          <img className='bg-primary w-full sm:max-w-72 rounded-lg' src={docInfo.image} alt="" />
+          <img className='[var(--primary)] w-full sm:max-w-72 rounded-lg' src={docInfo.image} alt="" />
         </div>
 
         <div className='flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0'>
@@ -103,6 +105,35 @@ const Appointment = () => {
             </p>
         </div>
       </div>
+
+      <div className='sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700'>
+        <p>Booking slots</p>
+        <div className='flex gap-3 items-center w-full overflow-x-scroll mt-4'>
+          {
+            docSlots.length && docSlots.map((item, index) => (
+              <div onClick={() => setSlotIndex(index)} className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? `bg-[var(--primary)] text-white` : 'border border-gray-200'}`} key={index}>
+                <p>
+                  {item[0] && daysOfWeek[item[0].datetime.getDay()]}
+                </p>
+                <p>{item[0] && item[0].datetime.getDate()}</p>
+              </div>
+            ))
+          }
+          </div>
+
+          <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
+            {docSlots.length && docSlots[slotIndex].map((item, index) => (
+              <p onClick={() => setSlotTime(item.time)} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer : ${item.time === slotTime ? 'bg-[var(--primary)] text-white' : 'text-gray-400 border border-gray-300'}`} key={index}>
+                {item.time.toLowerCase()}
+              </p>
+            ))}
+          </div>
+          <button className='bg-[var(--primary)] text-white text-sm font-light px-14 py-3 rounded-full my-6'>
+            Book an appointment
+          </button>
+      </div>
+
+      <RelatedDoctors docId={docId} speciality={docInfo.speciality}/>
     </div>
   )
 }
